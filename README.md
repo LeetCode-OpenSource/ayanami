89957b759b254797405e81ae028c6d413eb0d29e
Runjuu <huang@runjuu.com>
2019-03-29 18:45:45 +0800

    feat(init): setting up the development environment

---
+++ README.md
@@ -0,0 +1,2 @@
+# [WIP]Ayanami
+A better way to react with state.
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+
+const { compilerOptions } = require('./tsconfig.json')
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+}
