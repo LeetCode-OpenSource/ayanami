7392fac59a7f279f1e3cfb8ec09bff996dcb97ab
Runjuu <huang@runjuu.com>
2019-03-30 17:32:12 +0800

    chore: setting up test environment

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils')
-
-const { compilerOptions } = require('./tsconfig.json')
-
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 }
