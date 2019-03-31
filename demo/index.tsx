105523e3fc33522ba1cb241748bc83166a1e633c
Runjuu <huang@runjuu.com>
2019-03-31 23:46:04 +0800

    chore: update demo

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Observable } from 'rxjs'
-import { map, withLatestFrom } from 'rxjs/operators'
+import { Observable, of } from 'rxjs'
+import { map, mergeMap, withLatestFrom } from 'rxjs/operators'
 
 import { Ayanami, Effect, EffectAction, Reducer } from '../src'
 
 interface State {
   count: number
+}
+
+interface TipsState {
+  tips: string
+}
+
+class Tips extends Ayanami<TipsState> {
+  defaultState = {
+    tips: '',
+  }
+
+  @Reducer()
+  showTipsWithReducer(tips: string) {
+    return { tips }
+  }
+
+  @Effect()
+  showTipsWithEffectAction(tips$: Observable<string>): Observable<EffectAction> {
+    return tips$.pipe(map((tips) => this.getActions().showTipsWithReducer(tips)))
+  }
 }
 
 class Count extends Ayanami<State> {
@@ -23,22 +43,29 @@ class Count extends Ayanami<State> {
   minus(count$: Observable<number>, state$: Observable<State>): Observable<EffectAction> {
     return count$.pipe(
       withLatestFrom(state$),
-      map(([subCount, state]) => this.setStateAction({ count: state.count - subCount })),
+      mergeMap(([subCount, state]) =>
+        of(
+          this.setStateAction({ count: state.count - subCount }),
+          Tips.shared()
+            .getActions()
+            .showTipsWithEffectAction(`click minus button at ${Date.now()}`),
+        ),
+      ),
     )
   }
 }
 
 function CountComponent() {
-  const [state, actions] = Count.useHooks()
+  const [{ count }, actions] = Count.useHooks()
+  const [{ tips }] = Tips.useHooks()
 
   const add = (count: number) => () => actions.add(count)
   const minus = (count: number) => () => actions.minus(count)
 
   return (
     <div>
-      <p>
-        current count is <span>{state.count}</span>
-      </p>
+      <p>count: {count}</p>
+      <p>tips: {tips}</p>
       <button id="add" onClick={add(1)}>
         add one
       </button>
