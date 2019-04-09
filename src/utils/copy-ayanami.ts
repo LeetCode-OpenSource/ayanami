ea888c73544eece72ac30b9a9b6b901365becf47
Runjuu <huang@runjuu.com>
2019-04-09 15:47:50 +0800

    fix(utils/copyAyanami): types issues

--- src/utils/copy-ayanami.ts
+++ src/utils/copy-ayanami.ts
@@ -3,11 +3,11 @@ import { ConstructorOf, ConstructorOfAyanami } from '../types'
 
 import { setup } from './setup-ayanami'
 
-export function copyAyanami<M extends Ayanami<State>, State>(ayanamiConstructor: ConstructorOf<M>) {
+export function copyAyanami<M extends Ayanami<any>>(ayanamiConstructor: ConstructorOf<M>) {
   const Constructor = ayanamiConstructor as ConstructorOfAyanami<M, any>
   const ayanami = Constructor.getInstance()
 
   setup(ayanami)
 
-  return ayanami as M extends Ayanami<infer S> ? (S extends State ? M : never) : never
+  return ayanami as M extends Ayanami<infer _S> ? M : never
 }
