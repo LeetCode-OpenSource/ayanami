351a92e2caea0b813c29084297d2fead5d434fc9
Runjuu <huang@runjuu.com>
2019-04-09 12:57:26 +0800

    feat: getAllActionsForTest support instance

--- src/utils/test-helper.ts
+++ src/utils/test-helper.ts
@@ -4,8 +4,10 @@ import { ConstructorOf } from '../types'
 import { getAllActions } from './action-related'
 import { sharedAyanami } from './shared-ayanami'
 
-export function getAllActionsForTest<A extends Ayanami<S>, S>(
-  ayanamiConstructor: ConstructorOf<A>,
-) {
-  return getAllActions<A, S>(sharedAyanami(ayanamiConstructor))
+export function getAllActionsForTest<A extends Ayanami<S>, S>(ayanami: ConstructorOf<A> | A) {
+  if (ayanami instanceof Ayanami) {
+    return getAllActions<A, S>(ayanami)
+  } else {
+    return getAllActions<A, S>(sharedAyanami(ayanami))
+  }
 }
--- test/specs/ayanami.spec.ts
+++ test/specs/ayanami.spec.ts
@@ -1,39 +1,36 @@
-import { Ayanami, Reducer, getAllActionsForTest } from '../../src'
+import { Ayanami, Reducer, getAllActionsForTest, copyAyanami } from '../../src'
 
 interface CountState {
   count: number
 }
 
+class CountModel extends Ayanami<CountState> {
+  defaultState = { count: 0 }
+
+  @Reducer()
+  setCount(count: number): Partial<CountState> {
+    return { count }
+  }
+}
+
 describe('Ayanami specs:', () => {
+  let countModel: CountModel
+
+  beforeEach(() => {
+    countModel = copyAyanami(CountModel)
+  })
+
   it('getState', () => {
-    class Count extends Ayanami<CountState> {
-      defaultState = { count: 0 }
+    const actions = getAllActionsForTest(countModel)
 
-      @Reducer()
-      setCount(count: number): Partial<CountState> {
-        return { count }
-      }
-    }
-
-    const actions = getAllActionsForTest(Count)
-
-    expect(Count.getState()).toEqual({ count: 0 })
+    expect(countModel.getState()).toEqual({ count: 0 })
     actions.setCount(10)
-    expect(Count.getState()).toEqual({ count: 10 })
+    expect(countModel.getState()).toEqual({ count: 10 })
   })
 
   it('getState$', () => {
-    class Count extends Ayanami<CountState> {
-      defaultState = { count: 0 }
-
-      @Reducer()
-      setCount(count: number) {
-        return { count }
-      }
-    }
-
-    const actions = getAllActionsForTest(Count)
-    const count$ = Count.getState$()
+    const actions = getAllActionsForTest(countModel)
+    const count$ = countModel.getState$()
 
     const callback = jest.fn()
 
