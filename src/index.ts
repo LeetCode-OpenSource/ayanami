93b9a47e9b2298e66853e8175300a67619d98499
Runjuu <huang@runjuu.com>
2019-04-09 12:57:42 +0800

    feat: export copyAyanami

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './ayanami'
 export { EffectAction, ActionMethodOfAyanami } from './types'
 export { Effect, Reducer } from './decorators'
-export { getAllActionsForTest } from './utils'
+export { getAllActionsForTest, copyAyanami } from './utils'
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+export * from './copy-ayanami'
 export * from './shared-ayanami'
 export * from './test-helper'
 export * from './action-related'
