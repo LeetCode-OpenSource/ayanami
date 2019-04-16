94081bb599a78676b60f882056e6bf2ab22263d0
Runjuu <huang@runjuu.com>
2019-04-16 19:27:26 +0800

    test(Ayanami): destroy

--- test/specs/ayanami.spec.ts
+++ test/specs/ayanami.spec.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@asuka/di'
 
-import { Ayanami, Reducer, getAllActionsForTest, copyAyanami } from '../../src'
+import {
+  Ayanami,
+  Reducer,
+  getAllActionsForTest,
+  copyAyanami,
+  ActionMethodOfAyanami,
+} from '../../src'
 
 interface CountState {
   count: number
@@ -18,21 +24,20 @@ class CountModel extends Ayanami<CountState> {
 
 describe('Ayanami specs:', () => {
   let countModel: CountModel
+  let actions: ActionMethodOfAyanami<CountModel, CountState>
 
   beforeEach(() => {
     countModel = copyAyanami(CountModel)
+    actions = getAllActionsForTest(countModel)
   })
 
   it('getState', () => {
-    const actions = getAllActionsForTest(countModel)
-
     expect(countModel.getState()).toEqual({ count: 0 })
     actions.setCount(10)
     expect(countModel.getState()).toEqual({ count: 10 })
   })
 
   it('getState$', () => {
-    const actions = getAllActionsForTest(countModel)
     const count$ = countModel.getState$()
 
     const callback = jest.fn()
@@ -44,5 +49,11 @@ describe('Ayanami specs:', () => {
     expect(callback.mock.calls.length).toBe(2)
     expect(callback.mock.calls[0][0]).toEqual({ count: 0 })
     expect(callback.mock.calls[1][0]).toEqual({ count: 44 })
+  })
+
+  it('destroy', () => {
+    countModel.destroy()
+    actions.setCount(10)
+    expect(countModel.getState()).toEqual({ count: 0 })
   })
 })
