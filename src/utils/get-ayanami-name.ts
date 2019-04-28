c4d4366ad983baeb567de752a8bb6dbd53021563
Runjuu <huang@runjuu.com>
2019-04-28 20:50:52 +0800

    feat: `getAyanamiName` support pass constructor

--- src/utils/get-ayanami-name.ts
+++ src/utils/get-ayanami-name.ts
@@ -1,5 +1,10 @@
 import { Ayanami } from '../ayanami'
+import { ConstructorOf } from '../types'
 
-export function getAyanamiName(ayanami: Ayanami<any>): string {
-  return ayanami.constructor.name
+export function getAyanamiName(ayanami: Ayanami<any> | ConstructorOf<Ayanami<any>>): string {
+  if (Ayanami.isPrototypeOf(ayanami)) {
+    return (ayanami as Function).name
+  } else {
+    return ayanami.constructor.name
+  }
 }
