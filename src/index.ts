61ca1f3219b63d82964b05d10e6e72b35edc9603
Runjuu <huang@runjuu.com>
2019-04-28 21:59:54 +0800

    feat: remove unnecessary exported variable

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata'
 
 export * from './ayanami'
-export * from './decorators'
+export { Reducer, Effect, DefineAction, Singleton, Transient } from './decorators'
 export { EffectAction, ActionMethodOfAyanami } from './types'
 export { getAllActionsForTest } from './test-helper'
