1f9d45fa5cabd9f8d9cabcbe7a7123dfd8a52fd6
Runjuu <huang@runjuu.com>
2019-05-14 11:26:58 +0800

    feat: export TransientScope and SingletonScope

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import 'reflect-metadata'
 
-export { Ayanami, Reducer, Effect, DefineAction, EffectAction, ActionMethodOfAyanami } from './core'
+export {
+  Ayanami,
+  Reducer,
+  Effect,
+  DefineAction,
+  EffectAction,
+  ActionMethodOfAyanami,
+  TransientScope,
+  SingletonScope,
+} from './core'
 export { getAllActionsForTest } from './test-helper'
 export * from './hooks'
 export * from './connect'
