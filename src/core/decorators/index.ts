6eb2c04a9d8a4e0fe4c6ba4fd1885b6c08ca22f3
Runjuu <huang@runjuu.com>
2019-05-17 15:04:31 +0800

    feat(decorators): add types to Reducer and Effect

--- src/core/decorators/index.ts
+++ src/core/decorators/index.ts
@@ -1,9 +1,21 @@
-import { defineActionSymbols, effectSymbols, reducerSymbols } from '../symbols'
+import { Observable } from 'rxjs'
 
+import { defineActionSymbols, effectSymbols, reducerSymbols } from '../symbols'
+import { EffectAction } from '../types'
 import { createActionDecorator } from './action-related'
 
 export * from './action-related'
 
-export const Reducer = createActionDecorator(reducerSymbols)
-export const Effect = createActionDecorator(effectSymbols)
+interface DecoratorReturnType<V> {
+  (target: any, propertyKey: string, descriptor: { value?: V; get?(): V }): void
+}
+
+export const Reducer: <S = any>() => DecoratorReturnType<
+  (state: S, params?: any) => S
+> = createActionDecorator(reducerSymbols)
+
+export const Effect: <A = any, S = any>() => DecoratorReturnType<
+  (action: Observable<A>, state$: Observable<S>) => Observable<EffectAction>
+> = createActionDecorator(effectSymbols)
+
 export const DefineAction = createActionDecorator(defineActionSymbols)
