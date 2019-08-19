5016d19dd523e4e45632b020bdcd4acc548648f9
LongYinan <lynweklm@gmail.com>
2019-08-19 19:27:46 +0800

    feat(ssr): implment SSRServerOnly decorator

--- src/ssr/express.ts
+++ src/ssr/express.ts
@@ -8,6 +8,16 @@ import { Effect } from '../core/decorators'
 
 export const SKIP_SYMBOL = Symbol('skip')
 
+function addDecorator(target: any, method: any, middleware: any) {
+  const existedMetas = Reflect.getMetadata(SSRSymbol, target)
+  const meta = { action: method, middleware }
+  if (existedMetas) {
+    existedMetas.push(meta)
+  } else {
+    Reflect.defineMetadata(SSRSymbol, [meta], target)
+  }
+}
+
 export function SSR<T, Payload>(
   middleware?: (
     req: Request,
@@ -15,13 +25,7 @@ export function SSR<T, Payload>(
   ) => Payload | Promise<Payload> | typeof SKIP_SYMBOL,
 ) {
   return (target: T, method: string, descriptor: PropertyDescriptor) => {
-    const existedMetas = Reflect.getMetadata(SSRSymbol, target)
-    const meta = { action: method, middleware }
-    if (existedMetas) {
-      existedMetas.push(meta)
-    } else {
-      Reflect.defineMetadata(SSRSymbol, [meta], target)
-    }
+    addDecorator(target, method, middleware)
     if (!SSREnabled) {
       const originalValue = descriptor.value
       descriptor.value = function(
@@ -35,6 +39,18 @@ export function SSR<T, Payload>(
         return originalValue.call(this, action$, state$)
       }
     }
+    return Effect()(target, method, descriptor)
+  }
+}
+
+export function SSRServerOnly<T, Payload>(
+  middleware?: (
+    req: Request,
+    skip: () => typeof SKIP_SYMBOL,
+  ) => Payload | Promise<Payload> | typeof SKIP_SYMBOL,
+) {
+  return (target: T, method: string, descriptor: PropertyDescriptor) => {
+    addDecorator(target, method, middleware)
     return Effect()(target, method, descriptor)
   }
 }
