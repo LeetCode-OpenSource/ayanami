85c17d2effa9da1844ee691126d84b9d386b1d55
LongYinan <lynweklm@gmail.com>
2020-01-09 23:39:05 +0800

    doc: build documents on docz

---
+++ docs/doczrc.js
@@ -0,0 +1,73 @@
+export default {
+  typescript: true,
+  files: './**/*.mdx',
+  title: 'Ayanami document',
+  dest: 'public',
+  menu: [
+    'Introduction',
+    {
+      name: 'Basic',
+      menu: [
+        'Concept',
+        {
+          name: 'Action',
+          route: '/en/basic/action',
+        },
+        {
+          name: 'Effects',
+          route: '/en/basic/effects',
+        },
+        {
+          name: 'Dependencies Injection',
+          route: '/en/basic/dependencies-injection'
+        }
+      ],
+    },
+    {
+      name: 'Recipes',
+      menu: [
+        'Cancellation',
+        'Loading/Error states handling',
+        'Dependencies Replacement',
+        'Adding new module asynchronously',
+        'Writting tests',
+      ],
+    },
+    {
+      name: 'FAQ',
+      route: '/en/faq'
+    },
+    {
+      name: '基本概念',
+      menu: [
+        '概念',
+        {
+          name: 'Action',
+          route: '/zh/basic/action',
+        },
+        {
+          name: 'Effects',
+          route: '/zh/basic/effects',
+        },
+        {
+          name: '依赖注入',
+          route: '/zh/basic/dependencies-injection'
+        }
+      ],
+    },
+    {
+      name: '深入',
+      menu: [
+        '取消',
+        '处理 Loading/Error 状态',
+        '依赖替换',
+        'Module 异步加载',
+        '测试',
+      ],
+    },
+    {
+      name: '常见问题',
+      route: '/zh/faq'
+    },
+  ],
+}
--- docs/recipes/writting_tests.md
+++
@@ -1,4 +0,0 @@
-# Writting tests
-Yet ayanami is depending on **DI**, writting tests for ayanami **Module** is very simple:
-
-[Codesandbox](https://codesandbox.io/s/ayanami-recipes-writting-tests-4znlp)
